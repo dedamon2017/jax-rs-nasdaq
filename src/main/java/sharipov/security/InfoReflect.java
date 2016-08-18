92254ed6c58da3d1c6f4152020eb6f8b473cb2ae
Sharipov <pavel.sharipov@gmail.com>
2016-08-18 16:16:22 +0300

    Array in method

--- src/main/java/sharipov/security/InfoReflect.java
+++ src/main/java/sharipov/security/InfoReflect.java
@@ -7,6 +7,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 public class InfoReflect {
+
 	private Document document;
 
 	public InfoReflect(Document document) {
@@ -21,33 +22,25 @@ public class InfoReflect {
 		return document.getElementById(string);
 	}
 
-	static public class Command {
+	public static class Command {
 		private Object receiver;
 		private Method action;
 		private Object[] args;
 
-		public Command(Object object, String methodName, Object[] arguments) {
+		public Command(Object object, String methodName, Object[] arguments) throws NoSuchMethodException {
 			receiver = object;
 			args = arguments;
 			Class<? extends Object> cls = object.getClass();
 			Class[] argTypes = new Class[args.length];
-			for (int i = 0; i < args.length; i++)
+			for (int i = 0; i < args.length; i++) {
 				argTypes[i] = args[i].getClass();
-			try {
-				action = cls.getMethod(methodName, argTypes);
-			} catch (NoSuchMethodException exception) {
-				throw new AppException(exception.getMessage());
 			}
+			action = cls.getMethod(methodName, argTypes);
+			
 		}
 
-		public Object execute() {
-			try {
-				return action.invoke(receiver, args);
-			} catch (IllegalAccessException exception) {
-				throw new AppException(exception.getMessage());
-			} catch (InvocationTargetException exception) {
-				throw new AppException(exception.getMessage());
-			}
+		public Object execute() throws IllegalAccessException, InvocationTargetException {
+			return action.invoke(receiver, args);
 		}
 	}
 }
--- src/main/java/sharipov/security/SecurityService.java
+++ src/main/java/sharipov/security/SecurityService.java
@@ -1,7 +1,9 @@
 package sharipov.security;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Objects;
+import java.util.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
 
@@ -11,14 +13,15 @@ import org.jsoup.nodes.Element;
 
 @ApplicationScoped
 public class SecurityService {
+	private static final Logger LOGGER = Logger.getLogger(SecurityService.class.getName());
 	private static final String URL_FORMAT = "http://www.nasdaq.com/symbol/%s/real-time";
 	private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36";
 	private static final String PRICE_CLASS = "qwidget-dollar";
 	private static final String COMPANY_ID = "qwidget_pageheader";
 	private static final String DATE_ID = "qwidget_markettime";
 	private static final String GET_ELEMENT_BY_ID = "getElementById";
-	private static final String GET_ELEMENT_BY_CLASS = "getElementByClass";
-	
+	private static final String GET_ELEMENT_BY_CLASS = "getElementsByClass";
+
 	private Document document;
 	private Element element;
 	private Security security;
@@ -28,8 +31,9 @@ public class SecurityService {
 		return Jsoup.connect(url).userAgent(USER_AGENT).get();
 	}
 
-	public String getInfo(String elementName, String methodName) {
-		element = (Element) new InfoReflect.Command(new InfoReflect(document), methodName, new String[]{elementName}).execute();
+	public String getInfo(String[] elementsName, String methodName)
+			throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+		element = (Element) new InfoReflect.Command(new InfoReflect(document), methodName, elementsName).execute();
 		return Objects.requireNonNull(element.text());
 	}
 
@@ -37,12 +41,25 @@ public class SecurityService {
 		try {
 			document = getDocument(ticker);
 		} catch (IOException e) {
-			throw new AppException("Could not parse documnet.");
+			throw new AppException("Could not parse documnet.", e);
 		}
 		security = new Security();
-		security.setCompany(getInfo(COMPANY_ID, GET_ELEMENT_BY_ID));
-		security.setPrice(getInfo(PRICE_CLASS, GET_ELEMENT_BY_CLASS));
-		security.setDate(getInfo(DATE_ID, GET_ELEMENT_BY_ID));
+
+		try {
+			security.setCompany(getInfo(new String[] { COMPANY_ID }, GET_ELEMENT_BY_ID));
+			security.setPrice(getInfo(new String[] { PRICE_CLASS }, GET_ELEMENT_BY_CLASS));
+			security.setDate(getInfo(new String[] { DATE_ID }, GET_ELEMENT_BY_ID));
+		} catch (IllegalAccessException e) {
+			LOGGER.info(e.toString());
+			throw new AppException("Could not parse documnet.", e);
+		} catch (InvocationTargetException e) {
+			LOGGER.info(e.toString());
+			throw new AppException("Could not parse documnet.", e);
+		} catch (NoSuchMethodException e) {
+			LOGGER.info(e.toString());
+			throw new AppException("Could not parse documnet.", e);
+		}
+
 		security.setTicker(ticker);
 		return security;
 	}
